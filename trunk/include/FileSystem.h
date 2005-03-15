6f2800c78b4baa2c9eb12d1e7fc0d49d53445938
jpohlmann <jpohlmann@9e087ace-d9ef-0310-9923-fd35e469a289>
2005-03-15 15:43:34 +0000

    Documentation for FS functions added

--- trunk/include/FileSystem.h
+++ trunk/include/FileSystem.h
@@ -3,11 +3,46 @@
 
 #include <fx.h>
 
+/*
+	Namespace: FS
+	
+	Namespace including methods for the use in cases where distinguishing
+	between platforms is of importance.
+*/
 namespace FS
 {
 
+	/* 
+		Function: dirSeparator
+		
+		Returns the directory seperator.
+		
+		Returns:
+			- Windows: *\\*
+			- Linux: */*
+	*/
 	FXString dirSeparator();
+	
+	/*
+		Function: hiddenName
+		
+		Returns name of hidden file/directory with name *str*
+		
+		Returns:
+			- Windows: *str*
+			- Linux: *.str*
+	*/
 	FXString hiddenName(const FXString& str);
+	
+	/*
+		Function: configFilename
+		
+		Appends config file suffix to given filename 
+		
+		Returns:
+			- Windows: *str.ini*
+			- Linux: *str*
+	*/
 	FXString configFilename(const FXString& str);
 
 }
